98f53775bfa74092e556bd6183ab0e04aaa528e7
Favouriteless <62647254+Favouriteless@users.noreply.github.com>
2023-08-21 02:32:40 +0100

    80m Radio GUI bugfixing/power button

--- src/main/java/com/arrl/radiocraft/client/screens/HFRadio80mScreen.java
+++ src/main/java/com/arrl/radiocraft/client/screens/HFRadio80mScreen.java
@@ -21,14 +21,11 @@ public class HFRadio80mScreen extends AbstractHFRadioScreen {
     @Override
     protected void init() {
         super.init();
+        addRenderableWidget(new ToggleButton(container.isPowered(), leftPos + 10, topPos + 188, 14, 17, 0, 203, widgetsTexture, 256, 256, this::onPressPower)); // Power button (temporary)
         addRenderableWidget(new ToggleButton(container.blockEntity.getSSBEnabled(), leftPos + 90, topPos + 54, 34, 19, -1, 38, widgetsTexture, 256, 256, this::onPressSSB)); // SSB Button
         addRenderableWidget(new ToggleButton(container.blockEntity.getCWEnabled(), leftPos + 90, topPos + 74, 34, 19, -1, 0, widgetsTexture, 256, 256, this::onPressCW)); // CW Button
-
         addRenderableWidget(new HoldButton(leftPos + 139, topPos + 163, 51, 19, -1, 76, widgetsTexture, 256, 256, this::onPressPTT, this::onReleasePTT)); // PTT button
-
         addRenderableWidget(new Dial(leftPos + 42, topPos + 156, 28, 33, 143, 0, widgetsTexture, 256, 256, this::onFrequencyDialUp, this::onFrequencyDialDown)); // Frequency Dial
-
-
         addRenderableWidget(new Dial(leftPos + 122, topPos + 186, 15, 17, 94, 0, widgetsTexture, 256, 256, this::doNothing, this::doNothing)); // Mic Gain dial
         addRenderableWidget(new Dial(leftPos + 160, topPos + 186, 15, 17, 94, 0, widgetsTexture, 256, 256, this::doNothing, this::doNothing)); // Gain dial
     }
@@ -40,20 +37,13 @@ public class HFRadio80mScreen extends AbstractHFRadioScreen {
             poseStack.pushPose(); // Push/pop allows you to add a set of transformations to the stack. Pushing starts a new set and popping reverts to the previous set.
             poseStack.scale(0.8F, 0.8F, 0.8F);
             float freqMhz = container.getFrequency() / 1000.0F; // Frequency is in kHz, divide by 1000 to get MHz
-            font.draw(poseStack, String.format("%.3f", freqMhz) + "MHz", (leftPos + 19) / 0.8F, (topPos + 22) / 0.8F, 0xFFFFFF); // Divide the positions rendered at by 1.5F as the entire pose was scaled by 1.5F.
+            font.draw(poseStack, String.format("%.3f", freqMhz) + "MHz", (leftPos + 19) / 0.8F, (topPos + 22) / 0.8F, 0xFFFFFF);
             poseStack.popPose(); // Reset pose stack. Will cause a memory leak if you push without popping.
         }
     }
 
     @Override
-    protected void renderAdditionalBg(PoseStack poseStack, float partialTicks, int x, int y) {
-        if(container.isPowered()) {
-            if(container.isTransmitting())
-                blit(poseStack, leftPos + 30, topPos + 15, 1, 148, 29, 15);
-            if(container.isReceiving())
-                blit(poseStack, leftPos + 59, topPos + 15, 30, 148, 29, 15);
-        }
-    }
+    protected void renderAdditionalBg(PoseStack poseStack, float partialTicks, int x, int y) {}
 
     @Override
     protected void renderLabels(PoseStack poseStack, int x, int y) {}
