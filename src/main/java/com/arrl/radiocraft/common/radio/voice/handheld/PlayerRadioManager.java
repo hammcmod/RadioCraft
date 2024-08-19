69d1bd1db94932cedfc7510a83e72091a5ef1f8c
Jared Dunbar <jrddunbr@ja4.org>
2024-08-18 21:28:19 -0400

    It now reaches the world
    * Disable code that crashes on player join/leave
    * Disable code that crashes when trying to tick the network objects

--- src/main/java/com/arrl/radiocraft/common/radio/voice/handheld/PlayerRadioManager.java
+++ src/main/java/com/arrl/radiocraft/common/radio/voice/handheld/PlayerRadioManager.java
@@ -20,20 +20,20 @@ public class PlayerRadioManager {
 
     @SubscribeEvent
     public static void onPlayerJoined(PlayerEvent.PlayerLoggedInEvent event) {
-        Player player = event.getEntity();
-        playerRadios.put(player.getUUID(), new PlayerRadio(player));
+        //Player player = event.getEntity();
+        //playerRadios.put(player.getUUID(), new PlayerRadio(player));
     }
 
     @SubscribeEvent
     public static void onPlayerLeft(PlayerEvent.PlayerLoggedOutEvent event) {
-        UUID uuid = event.getEntity().getUUID();
-        get(uuid).setPlayer(null);
-        playerRadios.remove(uuid);
+        //UUID uuid = event.getEntity().getUUID();
+        //get(uuid).setPlayer(null);
+        //playerRadios.remove(uuid);
     }
 
     @SubscribeEvent
     public static void onPlayerCloned(PlayerEvent.Clone event) {
-        get(event.getOriginal().getUUID()).setPlayer(event.getEntity()); // Ensure to update the player object when cloned (e.g. dimension change)
+        //get(event.getOriginal().getUUID()).setPlayer(event.getEntity()); // Ensure to update the player object when cloned (e.g. dimension change)
     }
 
 }
