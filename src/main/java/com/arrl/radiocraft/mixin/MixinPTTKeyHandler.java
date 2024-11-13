1259853220e48f00aa4bfb8e6f8ea8b54cf23d09
Jared Dunbar <jrddunbr@ja4.org>
2024-11-12 22:31:38 -0500

    Update mixin comment; we do want this mixin

--- src/main/java/com/arrl/radiocraft/mixin/MixinPTTKeyHandler.java
+++ src/main/java/com/arrl/radiocraft/mixin/MixinPTTKeyHandler.java
@@ -7,9 +7,17 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 /*
+TODO: Fix the mixin. This is required to be able to use the PTT functionality without requiring an actual key press
+This is used in the GUI pane for the radios; a PTT button on the GUI screen can be clicked in place of using the PTT key.
 
-TODO: Not sure why we needed a mixin for this. I think that there may be better ways to handle this.
+The source code from the simple voice chat mod:
+https://github.com/henkelmax/simple-voice-chat/blob/1.21.3/common/src/main/java/de/maxhenkel/voicechat/voice/client/PTTKeyHandler.java
 
+Reference the old Gradle here for how mixin was used:
+https://github.com/hammcmod/RadioCraft/blob/dev/build.gradle
+*/
+
+/*
 @Mixin(PTTKeyHandler.class)
 public class MixinPTTKeyHandler {
 
