727fa1f15df0946a2402f94ae10ea0521120c269
Jared Dunbar <jrddunbr@ja4.org>
2025-08-11 15:23:57 -0400

    Added player name to callsign storage
    * Allows offline players to be identified w/o UUID lookups

--- src/main/java/com/arrl/radiocraft/api/capabilities/PlayerCallsignData.java
+++ src/main/java/com/arrl/radiocraft/api/capabilities/PlayerCallsignData.java
@@ -1,9 +1,11 @@
 package com.arrl.radiocraft.api.capabilities;
 
+import org.jetbrains.annotations.Nullable;
+
 /**
  * Player Callsign Data Class
  * @param playerUUID The PlayerUUID of the license holder
  * @param callsign The Callsign of the player
  * @param licenseClass The License Class of the player. These will generally be TECHNICIAN, GENERAL, or AMATEUR_EXTRA.
  */
-public record PlayerCallsignData(String playerUUID, String callsign, LicenseClass licenseClass) {}
+public record PlayerCallsignData(String playerUUID, @Nullable String playerName, String callsign, LicenseClass licenseClass) {}
