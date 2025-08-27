b83d5ddd240232299629f38ea1b8518aa2929617
Jared Dunbar <jrddunbr@ja4.org>
2025-08-27 12:43:51 -0400

    Added Slim Jim antenna model (#69)

--- src/main/java/com/arrl/radiocraft/common/blocks/antennas/SlimJimAntennaBlock.java
+++ src/main/java/com/arrl/radiocraft/common/blocks/antennas/SlimJimAntennaBlock.java
@@ -1,16 +1,42 @@
 package com.arrl.radiocraft.common.blocks.antennas;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.DoubleBlockHalf;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
-public class SlimJimAntennaBlock extends AntennaCenterBlock {
+public class SlimJimAntennaBlock extends DoubleVHFAntennaBlock {
+    public static final VoxelShape TOP = Block.box(5.0D, 0.0D, 5.0D, 11.0D, 9.0D, 11.0D);
+    public static final VoxelShape BOTTOM = Block.box(5.0D, 0.0D, 5.0D, 11.0D, 16.0D, 11.0D);
+
     public SlimJimAntennaBlock(Properties properties) {
         super(properties);
+    }
+
+    @Override
+    public @NotNull VoxelShape getShape(@NotNull BlockState state, @NotNull BlockGetter level, @NotNull BlockPos pos, @NotNull CollisionContext context) {
+        if (state.getValue(HALF) == DoubleBlockHalf.LOWER) {
+            return BOTTOM;
+        }
+        return TOP;
+    }
+
+    @Override
+    protected @NotNull VoxelShape getCollisionShape(@NotNull BlockState state, @NotNull BlockGetter level, @NotNull BlockPos pos, @NotNull CollisionContext context) {
+        if (state.getValue(HALF) == DoubleBlockHalf.LOWER) {
+            return BOTTOM;
+        }
+        return TOP;
     }
 
     @Override
